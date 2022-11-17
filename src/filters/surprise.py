caee7dce7ee2c68fc8d96300452b1f1e168a0045
floriangolling <florian.golling@epitech.eu>
2022-11-17 07:38:04 +0100

    feat: add algorithm architecure to each function filtesr

--- src/filters/surprise.py
+++ src/filters/surprise.py
@@ -1,2 +1,24 @@
+from PIL import Image
+from datetime import datetime
+
 def surprise(image_path):
-    pass
+
+    # Open image even grayscale and convert it to RGBA
+
+    img = Image.open(image_path)
+    rbgimg = Image.new("RGBA", img.size)
+    rbgimg.paste(img)
+
+    # Do your algorithm
+
+
+    # This loop get all RGBA value for all pixel.
+    for i in range(rbgimg.size[0]):
+        for j in range(rbgimg.size[1]):
+            r, g, b, a = rbgimg.getpixel((i, j))
+            print(r, g , b, a)
+
+
+    # Save output.
+    dateString = datetime.now().strftime("%m%d%Y%H-%M-%S")
+    rbgimg.save('./output_' + dateString + '.png')
