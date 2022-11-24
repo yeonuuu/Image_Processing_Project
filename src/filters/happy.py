fa369d9fcc1f3cd8daa61f787208038925b7a44a
floriangolling <florian.golling@epitech.eu>
2022-11-24 08:12:47 +0100

    feat: happy filter

--- src/filters/happy.py
+++ src/filters/happy.py
@@ -42,7 +42,11 @@ def happy(image_path):
     for pixel in pixelAddBlue:
         r, g, b, a = rbgimg.getpixel((pixel["x"], pixel["y"]))
         newB = b + pixel["RGBA"][1] if g + pixel["RGBA"][2] < 255 else 255
-        rbgimg.putpixel([pixel["x"], pixel["y"]], (r, g, newB, a))
+        rbgimg.putpixel([pixel["x"], pixel["y"]], (r, g, newB, a)) 
     # Save output.
+    for i in range(0, rbgimg.size[0]):
+        for j in range(0, rbgimg.size[1]):
+            r, g, b, a = rbgimg.getpixel((i, j))
+            rbgimg.putpixel((i, j), (r + 20, g + 20, b + 20, a))
     dateString = datetime.now().strftime("%m%d%Y%H-%M-%S")
     rbgimg.save("./outputs/output_happy" + dateString + ".png")
